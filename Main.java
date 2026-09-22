af8cab1d7cab9740420845efad22786b78f5453e
prasanna11249A115 <11249A115@kanchiuniv.ac.in>
2026-09-22 15:37:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Arun";
+
+        ArrayList<String> subjects = new ArrayList<>();
+
+        subjects.add("Java");
+        subjects.add("Python");
+        subjects.add("Maths");
+
+        System.out.println("Name: " + name);
+        System.out.println("Subjects: " + subjects);
+    }
+}
+
